1f844bc46ff629ac625e4ed8000432764015f19e
Habib Afolabi <habibwritescode@gmail.com>
2021-05-23 16:50:38 +0100

    Add products

---
+++ src/components/ProductList.js
@@ -0,0 +1,50 @@
+import styled from 'styled-components'
+
+const ProductsBox = styled.div`
+  display: grid;
+  gap: 1.9rem 1.74rem;
+  grid-template-columns: repeat(auto-fit, 10.1rem);
+  justify-content: center;
+  margin-bottom: 10rem;
+  padding: 0 1.7rem 0 1.8rem;
+
+  @media only screen and (max-width: 23.4em) {
+    padding-left: 1rem;
+    padding-right: 1rem;
+    column-gap: 1rem;
+  }
+
+  @media only screen and (max-width: 21.4em) {
+    column-gap: 5rem;
+  }
+`
+const Description = styled.p`
+  color: #718596;
+`
+const Price = styled.span`
+  display: inline-block;
+  font-size: 1.2rem;
+  font-weight: 700;
+  margin: .2rem 0;
+`
+const MinimumQty = styled.span`
+  color: #56636D;
+`
+
+
+const ProductList = ({products}) => {
+  return (
+    <ProductsBox>
+      {products.map(product => (
+        <div>
+          <img src={product} alt="" />
+          <Description>Free sample small tote bag gucci fen...</Description>
+          <Price>₦900 - ₦1,500</Price>
+          <MinimumQty>MOQ 4 (pieces)</MinimumQty>
+        </div>
+      ))}
+    </ProductsBox>
+  )
+}
+
+export default ProductList
--- src/pages/Buy.js
+++ src/pages/Buy.js
@@ -2,8 +2,18 @@ import React from 'react'
 import Banner from '../components/Banner'
 import Features from '../components/Features'
 import Header from '../components/Header'
+import ProductList from '../components/ProductList'
 import SearchForm from '../components/SearchForm'
 import SecondaryNav from '../components/SecondaryNav'
+
+import curology from '../images/curology.svg'
+import sneakers from '../images/sneakers.svg'
+import coke from '../images/coke.svg'
+import coffee from '../images/coffee.svg'
+import paperBag from '../images/paper-bag.svg'
+import box from '../images/box.svg'
+
+const products = [curology, sneakers, coke, coffee, paperBag, box]
 
 const Buy = () => {
   return (
@@ -14,6 +24,7 @@ const Buy = () => {
       <main>
         <Banner/>
         <Features/>
+        <ProductList products={products} />
       </main>
     </>
   )
